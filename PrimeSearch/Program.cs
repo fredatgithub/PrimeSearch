7b312aebee879190dcb6ffd307d77d8cb5dc0ec5
Freddy Juhel <domaine75@gmail.com>
2016-11-15 21:56:15 +0100

    Initial commit

--- PrimeSearch/Program.cs
+++ PrimeSearch/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PrimeSearch
 {
-  class Program
+  internal class Program
   {
-    static void Main(string[] args)
+    private static void Main()
     {
+      for (ulong i = 2; i < 10001; i++)
+      {
+        if (IsPrime(i))
+        {
+          Console.WriteLine(i);
+        }
+      }
+
+      Console.WriteLine("Press a key to exit");
+      Console.ReadKey();
+    }
+
+    private static bool IsPrime(ulong number)
+    {
+      if (number <= 1)
+      {
+        return false;
+      }
+
+      if (number == 2 || number == 3 || number == 5)
+      {
+        return true;
+      }
+
+      if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0)
+      {
+        return false;
+      }
+
+      for (ulong i = 6; i < Math.Sqrt(number); i = i + 2)
+      {
+        if (number % i == 0)
+        {
+          return false;
+        }
+      }
+
+      return true;
     }
   }
 }
