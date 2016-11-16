613085abb72b811cc993c0aed9ee0a3aa7eebc82
Freddy Juhel <domaine75@gmail.com>
2016-11-16 13:51:08 +0100

    Add big integer method

--- PrimeSearch/Program.cs
+++ PrimeSearch/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
+
 namespace PrimeSearch
 {
+  using System.Data.SqlTypes;
+  using System.Numerics;
+
   internal class Program
   {
     private static void Main()
@@ -12,6 +16,16 @@ namespace PrimeSearch
         {
           Console.WriteLine(i);
         }
+      }
+
+      BigInteger number = new BigInteger(ulong.MaxValue);
+      if (IsPrime(number))
+      {
+        Console.WriteLine(number + " est premier");
+      }
+      else
+      {
+        Console.WriteLine(number + " n'est pas premier");
       }
 
       Console.WriteLine("Press a key to exit");
@@ -36,6 +50,40 @@ namespace PrimeSearch
       }
 
       for (ulong i = 6; i < Math.Sqrt(number); i = i + 2)
+      {
+        if (number % i == 0)
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    private static bool IsPrime(BigInteger number)
+    {
+      if (number.IsEven)
+      {
+        return false;
+      }
+
+      if (number.Sign == 0 || number.Sign == -1)
+      {
+        return false;
+      }
+
+      if (number == 2 || number == 3 || number == 5)
+      {
+        return true;
+      }
+
+      if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0)
+      {
+        return false;
+      }
+
+      var tmpSqr = Math.Exp(BigInteger.Log(number) / 2);
+      for (ulong i = 6; i < tmpSqr; i = i + 2)
       {
         if (number % i == 0)
         {
