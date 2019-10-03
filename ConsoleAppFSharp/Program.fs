bc0529dec81059201639d442b646313d46a29dad
Fred <domaine75@gmail.com>
2019-10-03 18:46:33 +0200

    Add several unit test and delete F# project

--- ConsoleAppFSharp/Program.fs
+++ ConsoleAppFSharp/Program.fs
@@ -1,6 +1,28 @@
 ﻿// En savoir plus sur F# sur le site http://fsharp.org
 // Voir le projet 'Didacticiel F#' pour obtenir de l'aide.
+//open System
+//namespace open System
+//[<EntryPoint>]
+//let main (argv : string[]) = 
+//  printfn "my first program"      
+//  0 // retourne du code de sortie entier
+
+//open System
+
+//[<EntryPoint>]
+//let main argv =
+//    printfn "Hello World from F#!"
+//    0 // return an integer exit code
+
+//open System
+
+// Define a new function to print a name.
+// It is defined above the main function.
+let printGreeting name =
+    printfn "Hello %s from F#!" name
 
 [<EntryPoint>]
-let main argv = printfn "%A" argv
-    0 // retourne du code de sortie entier
+let main argv =
+    // Call your new function!
+    printGreeting "Ana"
+    0 // return an integer exit code
---
+++ UnitTestPrimeToolsLibrary/UnitTestPrimes.cs
@@ -0,0 +1,198 @@
+﻿using PrimeToolsLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPrimeToolsLibrary
+{
+  [TestClass]
+  public class UnitTestPrimes
+  {
+    [TestMethod]
+    public void TestMethod_IsPrime_0()
+    {
+      const int source = 0;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_1()
+    {
+      const int source = 1;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_2()
+    {
+      const int source = 2;
+      const bool expected = true;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_3()
+    {
+      const int source = 3;
+      const bool expected = true;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_4()
+    {
+      const int source = 4;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_5()
+    {
+      const int source = 5;
+      const bool expected = true;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_6()
+    {
+      const int source = 6;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_7()
+    {
+      const int source = 7;
+      const bool expected = true;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_8()
+    {
+      const int source = 8;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_9()
+    {
+      const int source = 9;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_10()
+    {
+      const int source = 10;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_11()
+    {
+      const int source = 11;
+      const bool expected = true;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_12()
+    {
+      const int source = 12;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_13()
+    {
+      const int source = 13;
+      const bool expected = true;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_14()
+    {
+      const int source = 14;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_15()
+    {
+      const int source = 15;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_16()
+    {
+      const int source = 16;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_17()
+    {
+      const int source = 17;
+      const bool expected = true;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_18()
+    {
+      const int source = 18;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_19()
+    {
+      const int source = 19;
+      const bool expected = true;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_IsPrime_20()
+    {
+      const int source = 20;
+      const bool expected = false;
+      bool result = PrimeTools.IsPrime(source);
+      Assert.AreEqual(result, expected);
+    }
+  }
+}
